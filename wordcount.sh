837091aede8f6e73537e7148669f0883f97de06e
Carles Amigó <fr3nd@fr3nd.net>
2016-06-22 19:50:07 +0700

    using parallel instead of xargs
    for some reason xargs didn't always return the same results.. probably
    something related with stdout buffers?

--- wordcount.sh
+++ wordcount.sh
@@ -8,13 +8,16 @@
 
 . ./config
 
+which docker-machine > /dev/null || (echo "docker-machine is needed to continue. Please install it..."; exit 1)
+which parallel > /dev/null || (echo "GNU parallel is needed to continue. Please install it..."; exit 1)
+
 eval $(docker-machine env manager)
 export DOCKER_HOST=$(docker-machine ip manager):3376
 
 TEMPDIR=$(mktemp --directory)
 split -l $SPLIT_LINES - $TEMPDIR/wordcount-
-find $TEMPDIR -type f -print0 \
-  | xargs -0 -I {} --max-procs=$MAX_MAP_PROCS -n 1 sh -c "cat {} | docker run --rm -i fr3nd/wordcount map.py" \
+find $TEMPDIR -type f \
+  | parallel -j$MAX_MAP_PROCS --no-notice "cat {} | docker run --rm -i fr3nd/wordcount map.py" \
   | sort \
   | docker run --rm -i fr3nd/wordcount reduce.py \
   | sort -n -k2 -r
