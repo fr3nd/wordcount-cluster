e84ceef55b76a35da1bb78db0bf14bae587823e5
Carles Amigó <fr3nd@fr3nd.net>
2016-06-22 19:32:13 +0700

    using print0 to be more reliable

--- wordcount.sh
+++ wordcount.sh
@@ -13,8 +13,8 @@ export DOCKER_HOST=$(docker-machine ip manager):3376
 
 TEMPDIR=$(mktemp --directory)
 split -l $SPLIT_LINES - $TEMPDIR/wordcount-
-find $TEMPDIR -type f \
-  | xargs -I {} --max-procs=$MAX_MAP_PROCS -n 1 sh -c "cat {} | docker run --rm -i fr3nd/wordcount map.py" \
+find $TEMPDIR -type f -print0 \
+  | xargs -0 -I {} --max-procs=$MAX_MAP_PROCS -n 1 sh -c "cat {} | docker run --rm -i fr3nd/wordcount map.py" \
   | sort \
   | docker run --rm -i fr3nd/wordcount reduce.py \
   | sort -n -k2 -r
